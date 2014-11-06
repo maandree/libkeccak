bbdbe2e09de0fd7586dbb5afa3a1fa64e143cf41
Mattias Andrée <maandree@operamail.com>
2014-11-06 22:11:14 +0100

    add todo
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/test.c
+++ src/test.c
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
+/* TODO we need to test other state sizes */
 #include <libkeccak.h>
 
 #include <stdio.h>
