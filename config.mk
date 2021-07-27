9110c8b7eba76e40cf66456d78765be86ec232b4
Mattias Andrée <maandree@kth.se>
2021-07-27 16:07:55 +0200

    config.mk: doc ALLOCA_LIMIT, add CC=cc, and set PREFIX=/usr
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- config.mk
+++ config.mk
@@ -1,6 +1,14 @@
-PREFIX = /usr/local
+PREFIX    = /usr
 MANPREFIX = $(PREFIX)/share/man
+
+CC = cc
 
 CFLAGS   = -std=c99 -O3
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
 LDFLAGS  = -s
+
+# You can add -DALLOCA_LIMIT=# to CPPFLAGS, where # is a size_t
+# value, to put a limit on how large allocation the library is
+# allowed to make with alloca(3). For buffers that can have any
+# size this limit will be used if it wants to allocate a larger
+# buffer. Choose 0 to use malloc(3) instead of alloca(3).
