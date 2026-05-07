af75e701c43fc4e754f516c81190b77fa4006bb8
Mattias Andrée <m@maandree.se>
2026-05-07 21:13:34 +0200

    Optimise conversion to hexadecimal
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- util/libkeccak_behex_lower.c
+++ util/libkeccak_behex_lower.c
@@ -12,11 +12,22 @@
 void
 libkeccak_behex_lower(char *restrict output, const void *restrict hashsum_, size_t n)
 {
+#define S(X)\
+	X"0", X"1", X"2", X"3", X"4", X"5", X"6", X"7",\
+	X"8", X"9", X"a", X"b", X"c", X"d", X"e", X"f"
+#if defined(__GNUC__)
+	__attribute__((__nonstring__))
+#endif
+	static const char lut[256][2] = {
+		S("0"), S("1"), S("2"), S("3"), S("4"), S("5"), S("6"), S("7"),
+		S("8"), S("9"), S("a"), S("b"), S("c"), S("d"), S("e"), S("f")
+	};
+
 	const unsigned char *restrict hashsum = hashsum_;
 
 	output[2 * n] = '\0';
 	while (n--) {
-		output[2 * n + 0] = "0123456789abcdef"[(hashsum[n] >> 4) & 15];
-		output[2 * n + 1] = "0123456789abcdef"[(hashsum[n] >> 0) & 15];
+		output[2 * n + 0] = lut[hashsum[n]][0];
+		output[2 * n + 1] = lut[hashsum[n]][1];
 	}
 }
--- util/libkeccak_behex_upper.c
+++ util/libkeccak_behex_upper.c
@@ -12,11 +12,22 @@
 void
 libkeccak_behex_upper(char *restrict output, const void *restrict hashsum_, size_t n)
 {
+#define S(X)\
+	X"0", X"1", X"2", X"3", X"4", X"5", X"6", X"7",\
+	X"8", X"9", X"A", X"B", X"C", X"D", X"E", X"F"
+#if defined(__GNUC__)
+	__attribute__((__nonstring__))
+#endif
+	static const char lut[256][2] = {
+		S("0"), S("1"), S("2"), S("3"), S("4"), S("5"), S("6"), S("7"),
+		S("8"), S("9"), S("A"), S("B"), S("C"), S("D"), S("E"), S("F")
+	};
+
 	const unsigned char *restrict hashsum = hashsum_;
 
 	output[2 * n] = '\0';
 	while (n--) {
-		output[2 * n + 0] = "0123456789ABCDEF"[(hashsum[n] >> 4) & 15];
-		output[2 * n + 1] = "0123456789ABCDEF"[(hashsum[n] >> 0) & 15];
+		output[2 * n + 0] = lut[hashsum[n]][0];
+		output[2 * n + 1] = lut[hashsum[n]][1];
 	}
 }
